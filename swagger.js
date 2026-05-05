88a789e35587dc06a1e50efe7574902a1e1ccc48
DAKS54 <sylvain.diallo@epitech.eu>
2026-05-05 12:46:00 +0000

    [Update] modify url to render url

--- swagger.js
+++ swagger.js
@@ -12,7 +12,7 @@ const options = {
     servers: [
       {
         // url: "http://localhost:3000",
-        url: "https://authback-six.vercel.app",
+        url: "https://authswagger.onrender.com/",
       },
     ],
   },
