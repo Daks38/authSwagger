ac547e92dd0a52f32cd4e253b0dd80b42ba27211
DAKS54 <sylvain.diallo@epitech.eu>
2026-05-05 13:47:25 +0000

    [Update] modify schem

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const User = require("../models/userRegister");
-const bcrypt = require("bcryptjs");
+const express = require("express")
+const User = require("../models/userRegister")
+const bcrypt  = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const secretKey = process.env.SECRET_KEY;
-const router = express.Router();
+const secretKey = process.env.SECRET_KEY
+const router = express.Router()
 
 /**
  * @swagger
@@ -16,12 +16,11 @@ const router = express.Router();
  *       required: true
  *       content:
  *         application/json:
- *           schema:{
+ *           schema:
  *             type: object
  *             required:
  *               - email
  *               - password
- * }
  *             properties:
  *               email:
  *                 type: string
@@ -29,7 +28,7 @@ const router = express.Router();
  *               password:
  *                 type: string
  *                 example: 12345
- *     responses:
+ *     responses: 
  *       200:
  *         description: Utilisateur connecté avec succès
  *         content:
@@ -44,50 +43,57 @@ const router = express.Router();
  *         description: Identifiants invalides
  */
 
+
 router.post("/", async (req, res) => {
-  const { email, password } = req.body;
+  const { email,password } = req.body;
   if (!email) {
-    return res.status(400).json({ msg: "Champ email est vide" });
+    return res.status(400).json({msg: "Champ email est vide"})
+    
+  }if (!password) {
+    return res.status(400).json({msg: "Champ mot de passe est vide"})
+    
   }
-  if (!password) {
-    return res.status(400).json({ msg: "Champ mot de passe est vide" });
-  }
-  const verifEmail = await User.findOne({ email });
+  const verifEmail  = await User.findOne({email});
   if (!verifEmail) {
-    return res.status(400).json({ msg: "Mot de passe ou email incorect" });
+    return res.status(400).json({msg: "Mot de passe ou email incorect"})
   }
-  const isPassword = await bcrypt.compare(password, verifEmail.password);
+      const isPassword = await bcrypt.compare(password, verifEmail.password)
   if (!isPassword) {
-    return res.status(400).json({ msg: "Mot de passe ou email incorect" });
-  }
-  const payload = {
-    isAdmin: verifEmail.isAdmin,
-    id: verifEmail.id,
-    name: verifEmail.name,
-    username: verifEmail.username,
-    number: verifEmail.number,
-    email: verifEmail.email,
-  };
-  const userInfos = {
-    isAdmin: verifEmail.isAdmin,
-    id: verifEmail.id,
-    isAdmin: verifEmail.isAdmin,
-    name: verifEmail.name,
-    username: verifEmail.username,
-    email: verifEmail.email,
-  };
-  //  const secretKey = "a"
-  jwt.sign(payload, secretKey, { expiresIn: 3600 }, (err, token) => {
-    if (err) {
-      return res.status(400).json(err.message);
-    } else {
-      return res.status(201).json({
-        users: userInfos,
-        token: token,
-        msg: "Vous êtes connecté avec succès",
-      });
-    }
-  });
+    return res.status(400).json({msg: "Mot de passe ou email incorect"})
+      }
+     const payload = {
+      isAdmin: verifEmail.isAdmin,
+      id: verifEmail.id,
+      name: verifEmail.name,
+      username: verifEmail.username,
+      number: verifEmail.number,
+      email: verifEmail.email
+     }
+     const userInfos = {
+      isAdmin: verifEmail.isAdmin,
+      id: verifEmail.id,
+      isAdmin: verifEmail.isAdmin,
+      name: verifEmail.name,
+      username: verifEmail.username,
+      email: verifEmail.email
+     }
+    //  const secretKey = "a"
+     jwt.sign( payload, secretKey,{expiresIn: 3600}, (err, token)=>{
+      if (err) {
+    return res.status(400).json(err.message)
+        
+      } else { 
+        
+        return res.status(201).json( {users: userInfos, token: token,msg:"Vous êtes connecté avec succès"});
+      
+      }
+     })
+
 });
 
-module.exports = router;
+
+
+
+
+
+module.exports = router 
